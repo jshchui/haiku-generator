b4b00449cfcd6b81d32bce675cba705e8f9f4171
jshchui <jshchui@gmail.com>
2019-02-25 22:38:59 -0800

    words no longer get removed when used for now

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ var Sentencer = require('sentencer');
 var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
 var multer = require('multer');
-var upload = multer({ dest: 'public/images'});
+var upload = multer({ dest: 'public/images' });
 
 const imageDirectory = './public/images/';
 
@@ -21,117 +21,140 @@ const vision = require('@google-cloud/vision');
 //   keyFilename: 'jackie-google-vision-key.json'
 // });
 
-
 const client = new vision.ImageAnnotatorClient({
   credentials: {
-    "type": process.env.type,
-    "project_id": process.env.project_id,
-    "private_key_id": process.env.private_key_id,
-    "private_key": process.env.private_key.replace(/\\n/g, '\n'),
-    "client_email": process.env.client_email,
-    "client_id": process.env.client_id,
-    "auth_uri": process.env.auth_uri,
-    "token_uri": process.env.token_uri,
-    "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-    "client_x509_cert_url": process.env.client_x509_cert_url
+    type: process.env.type,
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key.replace(/\\n/g, '\n'),
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    auth_uri: process.env.auth_uri,
+    token_uri: process.env.token_uri,
+    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+    client_x509_cert_url: process.env.client_x509_cert_url
   }
 });
 
 const fs = require('fs');
 
-router.post('/', upload.single('avatar'), function (req, res, next) {
+router.post('/', upload.single('avatar'), function(req, res, next) {
   let webWords;
   let labelWords;
 
   const pictureFile = req.file['filename'];
-  const webWordBank = webDetect(req.file['path']).then(results => {
-    webWords = results;
-    labelDetect(req.file['path']).then(labelResults => {
-      labelWords = labelResults;
-      const combinedWordBank = sortBySyllable([...new Set(results['arrayOfWords'].concat(labelResults))]);
+  const webWordBank = webDetect(req.file['path'])
+    .then(results => {
+      webWords = results;
+      labelDetect(req.file['path'])
+        .then(labelResults => {
+          labelWords = labelResults;
+          const combinedWordBank = sortBySyllable([
+            ...new Set(results['arrayOfWords'].concat(labelResults))
+          ]);
 
-      // best guess for web but not currently used
-      const bestGuessWeb = results['bestGuess'];
-      const wordBank = {}
-      wordBank['combinedWordBank'] = combinedWordBank;
-      // wordBank['wordForAdjective'] = bestGuessWeb;
-      let i = 0;
-      while( (labelResults[i].indexOf(' ') != -1) && i < labelResults.length) {
-        i++
-      }
+          // best guess for web but not currently used
+          const bestGuessWeb = results['bestGuess'];
+          const wordBank = {};
+          wordBank['combinedWordBank'] = combinedWordBank;
+          // wordBank['wordForAdjective'] = bestGuessWeb;
+          let i = 0;
+          while (
+            labelResults[i].indexOf(' ') != -1 &&
+            i < labelResults.length
+          ) {
+            i++;
+          }
 
-      wordBank['wordForAdjective'] = labelResults[i];
+          wordBank['wordForAdjective'] = labelResults[i];
 
-      return wordBank;
-    }).then(wordBank => {
+          return wordBank;
+        })
+        .then(wordBank => {
+          // const adjectives = sortBySyllable(generateAdjectives(10));
 
-      // const adjectives = sortBySyllable(generateAdjectives(10));
+          const bestGuessWord = wordBank['wordForAdjective'].trim();
+          const adjectiveOfBestWordObjects = makeRequest(
+            'GET',
+            `https://api.datamuse.com/words?rel_jjb=${
+              wordBank['wordForAdjective']
+            }`
+          )
+            .then(function(result) {
+              const arrayOfAdjectives = sortBySyllable(
+                JSON.parse(result).map(wordObject => {
+                  return wordObject['word'];
+                })
+              );
 
-      const bestGuessWord = wordBank['wordForAdjective'].trim();
-      const adjectiveOfBestWordObjects = makeRequest('GET', `https://api.datamuse.com/words?rel_jjb=${wordBank['wordForAdjective']}`)
-        .then(function (result) {
-          const arrayOfAdjectives = sortBySyllable(JSON.parse(result).map(wordObject => {
-            return wordObject['word'];
-          }));
+              const mySentence1 = generateSentence(
+                wordBank['combinedWordBank'],
+                5,
+                arrayOfAdjectives
+              );
+              const mySentence2 = generateSentence(
+                wordBank['combinedWordBank'],
+                7,
+                arrayOfAdjectives
+              );
+              const mySentence3 = generateSentence(
+                wordBank['combinedWordBank'],
+                5,
+                arrayOfAdjectives
+              );
 
-          const mySentence1 = generateSentence(wordBank['combinedWordBank'], 5, arrayOfAdjectives);
-          const mySentence2 = generateSentence(wordBank['combinedWordBank'], 7, arrayOfAdjectives);
-          const mySentence3 = generateSentence(wordBank['combinedWordBank'], 5, arrayOfAdjectives);
+              fs.readdir(imageDirectory, (err, files) => {
+                let imagesArray = [];
+                files.forEach(file => {
+                  imagesArray.push(file);
+                });
 
-          fs.readdir(imageDirectory, (err, files) => {
-            let imagesArray = []
-            files.forEach(file => {
-              imagesArray.push(file)
-            });
-
-            res.render('index', {
-              sentence1: mySentence1,
-              sentence2: mySentence2,
-              sentence3: mySentence3,
-              pictureURL: pictureFile,
-              webWords: webWords,
-              labelWords: labelWords,
-              imagesAmount: imagesArray.length
+                res.render('index', {
+                  sentence1: mySentence1,
+                  sentence2: mySentence2,
+                  sentence3: mySentence3,
+                  pictureURL: pictureFile,
+                  webWords: webWords,
+                  labelWords: labelWords,
+                  imagesAmount: imagesArray.length
+                });
+              });
             })
-          })
+            .catch(function(err) {
+              console.error('Error!', err);
+            });
         })
-        .catch(function (err) {
-          console.error('Error!', err);
-        })
+        .catch(err => {
+          console.error('ERROR:', err);
+        });
     })
     .catch(err => {
       console.error('ERROR:', err);
-    })
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  }); 
-})
-
+    });
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   fs.readdir(imageDirectory, (err, files) => {
-    let imagesArray = []
+    let imagesArray = [];
     files.forEach(file => {
-      imagesArray.push(file)
+      imagesArray.push(file);
     });
 
     res.render('index', {
       title: 'Haiku Generator',
       imagesInFolder: imagesArray,
       imagesAmount: imagesArray.length
-    })
-  })
+    });
+  });
 });
-
 
 function randomKeySelector(wordObj, maxSyllable) {
   // get keys which are all the syllables
   let keys = Object.keys(wordObj);
 
   // remove all syllables that are over maxSyllable
-  for(let i = 0; i < keys.length; i++) {
+  for (let i = 0; i < keys.length; i++) {
     if (parseInt(keys[i]) > parseInt(maxSyllable)) {
       keys.splice(i, 1);
       i--;
@@ -139,7 +162,7 @@ function randomKeySelector(wordObj, maxSyllable) {
   }
 
   // randomly select and return a an array of remaining syllables
-  return wordObj[keys[ keys.length * Math.random() << 0]];
+  return wordObj[keys[(keys.length * Math.random()) << 0]];
 }
 
 // recursivly generates sentence with total syllables given
@@ -154,26 +177,30 @@ function generateSentence(words, syllables, adjectives) {
   const firstWordSyllables = firstWord && syllable(firstWord);
 
   // this should remove the word from the word bank
-  const firstWordIndex = words[firstWordSyllables] && words[firstWordSyllables].indexOf(firstWord);
-  if ((firstWordIndex || firstWordIndex === 0) && (firstWordIndex !== -1)) {
-    words[firstWordSyllables].splice(firstWordIndex, 1);
-  }
+  // currently removing this function because it is causing a lot of undefines to return, will improve it shortly
+  // const firstWordIndex =
+  //   words[firstWordSyllables] && words[firstWordSyllables].indexOf(firstWord);
+  // if ((firstWordIndex || firstWordIndex === 0) && firstWordIndex !== -1) {
+  //   words[firstWordSyllables].splice(firstWordIndex, 1);
+  // }
 
   const remainingSyllables = syllables - firstWordSyllables;
   sentence += firstWord;
   if (remainingSyllables > 0) {
-    sentence = `${generateSentence(adjectives || words, remainingSyllables)} ${sentence}`;
+    sentence = `${generateSentence(
+      adjectives || words,
+      remainingSyllables
+    )} ${sentence}`;
   }
 
   return sentence;
 }
 
-
 // randomly generates adjectives from library sentencer
 function generateAdjectives(amount) {
   let adjectiveList = [];
-  for(let i = 0; i < amount; i++) {
-    adjectiveList.push(Sentencer.make("{{ adjective }}"));
+  for (let i = 0; i < amount; i++) {
+    adjectiveList.push(Sentencer.make('{{ adjective }}'));
   }
 
   return adjectiveList;
@@ -183,9 +210,9 @@ function generateAdjectives(amount) {
 function sortBySyllable(arrayOfWords) {
   sortedBySyllables = {};
 
-  arrayOfWords.forEach((word) => {
+  arrayOfWords.forEach(word => {
     const numberOfSyllables = syllable(word);
-    if(sortedBySyllables[numberOfSyllables]) {
+    if (sortedBySyllables[numberOfSyllables]) {
       sortedBySyllables[numberOfSyllables].push(word);
     } else {
       sortedBySyllables[numberOfSyllables] = [word];
@@ -193,7 +220,15 @@ function sortBySyllable(arrayOfWords) {
   });
 
   if (!sortedBySyllables['1']) {
-    sortedBySyllables['1'] = ['real', 'ill', 'lax', 'coy', 'prime', 'bored', 'fair'];
+    sortedBySyllables['1'] = [
+      'real',
+      'ill',
+      'lax',
+      'coy',
+      'prime',
+      'bored',
+      'fair'
+    ];
   }
 
   if (sortedBySyllables['0']) {
@@ -224,57 +259,55 @@ function labelDetect(imagePath) {
 
 // get words from web detection in google vision
 function webDetect(imagePath) {
-  return client
-    .webDetection(imagePath)
-    .then(results => { 
-      const webEntities = results[0].webDetection['webEntities'];
-      const bestGuessLabels = results[0].webDetection['bestGuessLabels'];
-      const arrayOfWords = [];
-      const webWordBank = {};
+  return client.webDetection(imagePath).then(results => {
+    const webEntities = results[0].webDetection['webEntities'];
+    const bestGuessLabels = results[0].webDetection['bestGuessLabels'];
+    const arrayOfWords = [];
+    const webWordBank = {};
 
-      console.log('webEntities: ', webEntities);
+    console.log('webEntities: ', webEntities);
 
-      webEntities.forEach(entity => {
-        arrayOfWords.push(entity.description.toLowerCase());
-      });
-      webWordBank['arrayOfWords'] = arrayOfWords;
-      webWordBank['bestGuess'] = bestGuessLabels[0].label;
-
-      // if best guess label is one word, use best guess label
-      // if not use the first word in the web entities
-      // if not then use the last word of the first word in best guess label
-      if(bestGuessLabels[0].label.indexOf(' ') === -1) {
-        webWordBank['bestGuess'] = bestGuessLabels[0].label;
-      } else if (arrayOfWords[0].indexOf(' ') === -1) {
-        webWordBank['bestGuess'] = arrayOfWords[0];
-      } else {
-        let bestGuessWordSplit = bestGuessLabels[0].label.split(' ');
-        webWordBank['bestGuess'] = bestGuessWordSplit[bestGuessWordSplit.length - 1];
-      }
-
-      // return arrayOfWords; 
-      return webWordBank; 
+    webEntities.forEach(entity => {
+      arrayOfWords.push(entity.description.toLowerCase());
     });
+    webWordBank['arrayOfWords'] = arrayOfWords;
+    webWordBank['bestGuess'] = bestGuessLabels[0].label;
+
+    // if best guess label is one word, use best guess label
+    // if not use the first word in the web entities
+    // if not then use the last word of the first word in best guess label
+    if (bestGuessLabels[0].label.indexOf(' ') === -1) {
+      webWordBank['bestGuess'] = bestGuessLabels[0].label;
+    } else if (arrayOfWords[0].indexOf(' ') === -1) {
+      webWordBank['bestGuess'] = arrayOfWords[0];
+    } else {
+      let bestGuessWordSplit = bestGuessLabels[0].label.split(' ');
+      webWordBank['bestGuess'] =
+        bestGuessWordSplit[bestGuessWordSplit.length - 1];
+    }
+
+    // return arrayOfWords;
+    return webWordBank;
+  });
 }
 
 function httpGet(url, callback) {
   let xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = function() {
-    if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-      if(callback) callback(xmlHttp.responseText);
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      if (callback) callback(xmlHttp.responseText);
       return null;
     }
-
-  }
+  };
   xmlHttp.open('GET', url, true);
-  xmlHttp.send( null );
+  xmlHttp.send(null);
 }
 
 function makeRequest(method, url) {
-  return new Promise(function (resolve, reject) {
+  return new Promise(function(resolve, reject) {
     var xhr = new XMLHttpRequest();
     xhr.open(method, url, true);
-    xhr.onload = function () {
+    xhr.onload = function() {
       if (this.status >= 200 && this.status < 300) {
         resolve(xhr.responseText);
       } else {
@@ -284,7 +317,7 @@ function makeRequest(method, url) {
         });
       }
     };
-    xhr.onerror = function () {
+    xhr.onerror = function() {
       reject({
         status: this.status,
         statusText: xhr.statusText
